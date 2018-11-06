821fead669d94e8ffaa9d57626c349addbaff107
Wasif Khan <waasif.khaan@gmail.com>
2018-11-06 16:41:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function setThisWithCall(fn,thisValue,arg) {
   return fn.call(thisValue,arg);
 }
 function setThisWithApply(fn,thisValue,arg){
-return fn.apply(thisValue,[arg]);
+return fn.apply(thisValue,arg);
 }
 function returnNewFunctionOf(fn,thisValue){
   return fn.bind(thisValue)
