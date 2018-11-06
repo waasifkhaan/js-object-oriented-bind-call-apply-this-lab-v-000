4e9aaf7998bd68d5871a177701f41928f1adf2ff
Wasif Khan <waasif.khaan@gmail.com>
2018-11-06 16:22:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //Your code here
+function justInvoke(returnsThisAndArgs){
+  return returnsThisAndArgs();
+}
+
+function setThisWithCall(fn,thisValue,arg) {
+  return fn.call(thisValue,arg);
+}
+function setThisWithApply(fn,thisValue,arg){
+return fn.apply(thisValue,arg);
+}
+function returnNewFunctionOf(fn,thisValue){
+  return fn.bind(thisValue)
+}
